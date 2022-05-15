2ec5576a5649ed23b460dc22e1df3c9c7fc46de9
Alan Hernandez <teccnoyil@gmail.com>
2022-05-15 17:32:26 -0500

    creacion de Scallford y buildcontext

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,11 +4,13 @@ class HomeScreen extends StatelessWidget{
   const HomeScreen({Key? key}) : super(key: key); //? Key nos sirve para identificar un widget propiamiente dentro de el contexto
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(  //! widget -- es una forma de trabjar que nos pide cual es nuestro widger que queremos mostrar acá
-       child:  Text("Hola desde HomeScreen"), //! widget 
-     );  
-    // TODO: implement build
+  Widget build(BuildContext context) { //? casi todos los widgets reciben un child o un children
+    return const Scaffold(  //? prepara nustra aplicacion para que empecemos a trabajar, LIENZO/PARA EMPEZAR A COLOR LOW WIDGETS
+    backgroundColor: Colors.cyan,
+    body:  Center(
+        child:  Text("HomeScreen"), //! widget 
+       )
+    );  
     //throw UnimplementedError(); //? throw => lanzar error implementado, en caso de eror lanza esto
   }
 
